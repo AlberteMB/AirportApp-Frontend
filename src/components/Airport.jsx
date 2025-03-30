be82fb70145d0fdbe15791aaa7322c0bf60c125c
AlberteMB <albertebardanca@hotmail.com>
2025-03-30 12:40:16 +0200

    Remaking Airport component

--- src/components/Airport.jsx
+++ src/components/Airport.jsx
@@ -1,35 +1,41 @@
 import { useEffect, useState } from "react";
 import ApiAirports from "../middleware/api-backend"; 
+import SpainMap from "./components/SpainMap";
 
-const Airport = ({ onAirportsLoaded }) => {
-    const [loading, setLoading] = useState(true);
+const Airport = () => {
+    const [airports, setAirports] = useState([]);
 
     useEffect(() => {
         const fetchAirports = async () => {
             try {                         
                 const response = await ApiAirports.get("/"); // Solicitud al endpoint
-                if (onAirportsLoaded) {
-                    onAirportsLoaded(response.data); // Pasar los datos al componente padre
-                }
+                setAirports(response.data);
             } catch (error) {
                 console.error("Error fetching airports:", error);
-                if (onAirportsLoaded) {
-                    onAirportsLoaded([]); // Enviar una lista vacía en caso de error
-                }
-            } finally {
-                setLoading(false); // Finaliza la carga
             }
         };
 
         fetchAirports();
-    }, [onAirportsLoaded]);
+    }, []);
 
-    if (loading) return <p>Cargando aeropuertos...</p>; // Mostrar indicador de carga
-
-    return null;
+    return (
+        <div>
+            <h2>Aeropuertos</h2>
+            <ul>
+                {airports.map((airport, index) => (
+                    <li key={index}>
+                        <strong>ID:</strong> {airport.id}, <strong>Nombre:</strong> {airport.name},
+                         <strong>Ciudad:</strong> {airport.city}, <strong><Region>{airport.region}</Region></strong>,
+                         <strong>Latitud:</strong> {airport.latitude}, <strong>Longitud:</strong> {airport.longitude}
+                    </li>
+                ))}
+            </ul>
+            <SpainMap airports={airports} />
+        </div>
+    );       
 };
-
 export default Airport;
 
 
 
+0
