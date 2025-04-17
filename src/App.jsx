1610a479c8a5b542d0c969f1e1fa4b0df0d0c075
AlberteMB <albertebardanca@hotmail.com>
2025-04-17 09:51:53 +0200

    adding paths to App. Creating template for components Update and Create Flight

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import './App.css';
 import Airport from "./components/Airport"; 
 import FlightRegister from "./components/FlightRegister";
+import CreateFlightForm from "./components/CreateFlightForm";
+import UpdateFlightForm from "./components/UpdateFlightForm";
 import { Box } from '@mui/material';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
@@ -10,13 +12,15 @@ function App() {
     <Box sx={{ display: 'flex', flexDirection: 'column', height: '100vh' }}>
       {/* Encabezado */}
       <Box sx={{ p: 2, backgroundColor: '#5e992c', textAlign: 'center' }}>
-        <h1>Mapa de Aeropuertos de España</h1>
+        <h1>Aeropuertos de España</h1>
       </Box>
 
       {/* Contenedor principal */}
       <Routes>
         <Route path="/" element={<Airport />} />
-        <Route path="/flights/:airportId" element={<FlightRegister />} />
+        <Route path="/flights/:id" element={<FlightRegister />} />
+        <Route path="/flights/create" element={<CreateFlightForm />} />
+        <Route path="/flights/update/:id" element={<UpdateFlightForm />} />
       </Routes>
       </Box>
         </Router>
---
+++ src/components/UpdateFlightForm.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import { useNavigate, useLocation } from "react-router-dom";
+import {
+  TextField,
+  Button,
+  Paper,
+  Select,
+  MenuItem,
+  FormControl,
+  InputLabel,
+  Box,
+  Typography
+} from "@mui/material";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import dayjs from "dayjs";
+import { useAppServices } from "../middleware/appServicesContext";
+
+const UpdateFlightForm = () => {
+
+
+    return (
+
+    );
+};
+
+export default UpdateFlightForm;
