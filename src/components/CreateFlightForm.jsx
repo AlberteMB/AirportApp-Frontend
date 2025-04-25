920934aa4ceba6b3fc500ed4580351eae8053c71
AlberteMB <albertebardanca@hotmail.com>
2025-04-25 17:34:28 +0200

    WIP CreateFlightForm

--- src/components/CreateFlightForm.jsx
+++ src/components/CreateFlightForm.jsx
@@ -18,7 +18,46 @@ import dayjs from "dayjs";
 import { useAppServices } from "../middleware/appServicesContext";
 
 const CreateFlightForm = () => {
+  const appService = useAppServices();
+  const location = useLocation();
+  const navigate = useNavigate();
+  const selectedAirport = location.state?.airport;
+  const { airportId } = useParams();
+  const { planes, airports } = location.state || {}; // Get planes and airports from location state
 
+  // Form data state
+  const [formData, setFormData] = useState({
+    flightNumber: "",
+    departureTime: dayjs(),
+    arrivalTime: dayjs(),
+    departureAirportId: airportId,
+    arrivalAirportId: "",
+    flightStatus: "",
+    plane: "",
+  });
+
+  // Handle input changes
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+  
+  const handleDateChange = (name, value) => {
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const flightData = {
+
+      }
+      await appService.flight.createFlight(formData);
+      navigate("/flights");
+    } catch (error) {
+      console.error("Error creating flight", error);
+    }
+  };
 
     return (
 
